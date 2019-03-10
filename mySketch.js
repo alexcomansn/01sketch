4c042a41b24d88226d0256da1e97097b7922ff88
alexcomansn <48419708+alexcomansn@users.noreply.github.com>
2019-03-10 23:41:20 +0100

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -4,7 +4,7 @@ const s = 300;
 const size = 200;
 
 function setup() {
-	createCanvas(1000, 1000, WEBGL);
+	createCanvas(200, 200, WEBGL);
 	angleMode(DEGREES);
 	colorMode(255, 255, 255, 100);
 	rectMode(CENTER);
